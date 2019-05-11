a988d719cbcdb144fd8d6dcf4fbffeb623b064ca
Yared <yared.tye@gmail.com>
2019-05-11 15:50:58 +0300

    stats and history api URL modified

--- emr/config/config.js
+++ emr/config/config.js
@@ -1,6 +1,7 @@
 const URL_AUTH = 'http://127.0.0.1:3001';
 const URL_ACCOUNT = 'http://127.0.0.1:3002';
-const URL_EMR = 'http://127.0.0.1:5560';
+const URL_EMR_HISTORY = 'http://127.0.0.1:5560';
+const URL_EMR_STATS = 'http://127.0.0.1:5561';
 
 const URI_STREAM='http://127.0.0.1:8890'
 
@@ -11,8 +12,12 @@ const account = {
   URL: URL_ACCOUNT+'/platform/accounts/v1'
 }
 
-const emr = {
-  URL: URL_EMR+'/app/emr/v1'
+const emr_history = {
+  URL: URL_EMR_HISTORY+'/app/emr/v1'
+}
+
+const emr_stats = {
+  URL: URL_EMR_STATS+'/app/emr/v1'
 }
 
 const stream = {
@@ -22,7 +27,8 @@ const stream = {
 const config = {
   AUTH: auth,
   ACCOUNT: account,
-  EMR: emr,
+  EMR_STATS: emr_stats,
+  EMR_HISTORY: emr_history,
   STREAM: stream
 }
 
