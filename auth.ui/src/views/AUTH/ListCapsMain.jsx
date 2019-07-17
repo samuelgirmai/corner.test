4d44f8c2a34a3f6425959dbcf602e2d66539d161
Samuel Girmay <samuel@bokri.xyz>
2019-07-17 15:46:38 +0300

    - list caps minor label modification

--- auth.ui/src/views/AUTH/ListCapsMain.jsx
+++ auth.ui/src/views/AUTH/ListCapsMain.jsx
@@ -30,8 +30,6 @@ import img5 from 'assets/img/blog-5.jpg';
 class ExtendedTables extends Component{
   constructor(props){
     super(props);
-    let rows = [];
-
     this.state = {
       caps: STORE.read('caps', null),
       selectService: null,
@@ -39,13 +37,12 @@ class ExtendedTables extends Component{
     }
 
     this.services = STORE.read('services', null);
-
   } 
 
   componentDidMount(){
     this.setState({
       selectService: this.getSelectService(),
-      service: _.filter(this.getSelectService(), (i) => i.label === 'auth')[0]
+      service: _.filter(this.getSelectService(), (i) => i.label === 'auth')[0],
     })
 
   }
@@ -82,95 +79,81 @@ class ExtendedTables extends Component{
   }
 
   list = () => {
-    let table = [];
+    let table = [], rows = [];
 
-    if(this.state.service)
+    if(this.state.service){
       table =_.filter(this.state.caps, (c) => c.service_id === this.state.service.value)
- 
-    let rows = [];
-        for(let i=0; i<table.length; i++){
-          rows.push(                                            
-            <tr key={i}>
-                <td className="text-left">{i+1}</td>
-                <td className="text-description">{table[i].desc}</td>
-                <td className="text-left">{table[i].uri}</td>
-                <td className="text-left">{table[i].cap_id}</td>
-                <td className="text-center">
-                <OverlayTrigger placement="top" overlay={this.del()}>
-                    <Button simple bsStyle="danger" bsSize="xs" onClick={()=> {this.onDeleteCap(table[i].service_id, table[i].cap_id)}}>
-                        <i className="fa fa-trash"></i>
-                    </Button>
-                </OverlayTrigger>
-              </td>
-            </tr>
-          )
-        }
-
-      return rows;
     }
 
-    selector = () => {
-      return (
-        <FormGroup>
-          <ControlLabel>Select a service ({this.state.caps.length})</ControlLabel>
-            <Select name="services" placeholder="Select Services"
-              closeOnSelect={true}
-              value={this.state.service}
-              options={this.state.selectService}
-              autoFocus={true}
-              onChange={(value) => this.onSelectService(value)}
-          />
-        </FormGroup>
-      );
+    for(let i=0; i<table.length; i++){
+      rows.push(                                            
+        <tr key={i}>
+          <td className="text-left">{i+1}</td>
+          <td className="text-description">{table[i].desc}</td>
+          <td className="text-left">{table[i].uri}</td>
+          <td className="text-left">{table[i].cap_id}</td>
+          <td className="text-center">
+            <OverlayTrigger placement="top" overlay={this.del()}>
+              <Button simple bsStyle="danger" bsSize="xs" onClick={()=> {this.onDeleteCap(table[i].service_id, table[i].cap_id)}}>
+                <i className="fa fa-trash"></i>
+              </Button>
+            </OverlayTrigger>
+          </td>
+        </tr>
+      )
     }
 
-    render(){
+    return rows;
+  }
 
-        return (
-            <div className="main-content">
-                <Grid fluid>
-                    <Row>
-                        <Col md={12}>
-                            <Card
-                                title={this.selector()}
-                                content={
-                                  <div>
-                                  {/*<Row>
-                                  <Col md={3}>
-                                      <FormGroup>
-                                          <ControlLabel>Select a service</ControlLabel>
-                                           <Select name="services" placeholder="Select Services"
-                                                closeOnSelect={true}
-                                                value={this.state.service}
-                                                options={this.state.selectService}
-                                                autoFocus={true}
-                                                onChange={(value) => this.onSelectService(value)}
-                                            />
-                                      </FormGroup>
-                                  </Col>
-                              </Row>*/}
-                                    <Table responsive>
-                                        <thead>
-                                            <tr>
-                                                <th className="text-center">#</th>
-				        	                              <th className="text-description">Cap Name</th>
-                                                <th className="text-left">Cap URI</th>
-                                                <th className="text-left">Cap ID</th>
-                                                <th className="text-center">Actions</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-					                                {this.list()}
-                                        </tbody>
-                                    </Table></div>
-                                }
-                            />
-                        </Col>
-                    </Row>
-                </Grid>
-            </div>
-        );
-    }
+  capServices = () => {
+    return (
+      <FormGroup>
+        <ControlLabel>Select a service (total = {this.state.caps.length} CAPS)</ControlLabel>
+          <Select name="services" placeholder="Select Services"
+          closeOnSelect={true}
+          value={this.state.service}
+          options={this.state.selectService}
+          autoFocus={true}
+          onChange={(value) => this.onSelectService(value)}
+        />
+      </FormGroup>
+    );
+  }
+
+  render(){
+    return (
+      <div className="main-content">
+        <Grid fluid>
+          <Row>
+            <Col md={12}>
+              <Card
+                title={this.capServices()}
+                content={
+                  <div>
+                    <Table responsive>
+                      <thead>
+                        <tr>
+                          <th className="text-center">#</th>
+                          <th className="text-description">Cap Name</th>
+                          <th className="text-left">Cap URI</th>
+                          <th className="text-left">Cap ID</th>
+                          <th className="text-center">Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.list()}
+                      </tbody>
+                    </Table>
+                  </div>
+                }
+              />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default ExtendedTables;
