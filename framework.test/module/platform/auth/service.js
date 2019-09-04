61995e854239886f85b669cf54084f8508c6c490
Yared <yared.tye@gmail.com>
2019-09-04 12:30:05 +0300

    - setup module decoupled into service.js, client.js, cap.js  - EMR client configuration added > ./config/clients.js

---
+++ framework.test/module/platform/auth/service.js
@@ -0,0 +1,138 @@
+import API from '../api/api_rest';
+import CONFIG from '../config/config'
+import {services} from '../config/services';
+import _ from 'lodash';
+
+var fs = require('fs');
+var index = 0;
+
+let CONF_DIR = '../../corner.backend/'
+
+export async function get_license()
+{
+  return CONFIG.auth.license
+}
+
+export async function get_sii()
+{
+  return services[index]? services[index].sii: null;
+}
+
+export function get_config(arg)
+{
+  let conf, sname;
+  let subdir = 'platform/'
+
+  if(!arg.sii)
+    return null;
+
+  if(arg.sii.name == 'p.stream.core')
+    return 'ignore';
+
+  sname = arg.sii.name.split('.', 2);
+
+  if(sname[0] === 'emr')
+    subdir = 'services/emr/';
+
+  conf = CONF_DIR + subdir + sname[1]+'/config/config.js'
+  
+  return conf;
+}
+
+export async function create_service(arg)
+{
+  let data, ret;
+
+  if(!arg || !arg.sii)
+    return null;
+
+  data = {
+    auth: {
+      license: arg.license,
+    }, 
+    param: {
+      sii: arg.sii
+    }
+  }
+
+  ret = await API.run(data, '/platform/auth/users/service/write');
+   
+  return ret.status == "ok"? ret.result.user_id: null;
+}
+
+export async function get_service(arg)
+{
+  let data, ret, s;
+
+  if(!arg)
+    return null;
+ 
+  data = {
+    auth: {
+      license: arg.license,
+    },
+  }
+
+  ret = await API.run(data, '/platform/auth/users/services/list');
+
+  if(ret.status === 'err')
+    return null;
+
+  s =  _.filter(ret.result.services, (o) => {
+    return (arg.user_id === o.user_id)
+  });
+
+  if(!s.length)
+    return null;
+
+  return s[0];
+}
+
+/* update service's  license and user_id 
+ * configuration wih in {SERVICE_PATH}/config/config
+ */
+export async function update_config(arg)
+{ 
+  let s = [], conf, buf;
+
+  if(!arg || !arg.service)
+    return null;
+
+  if(fs.existsSync(arg.conf))
+  {
+    buf = fs.readFileSync(arg.conf, 'utf8')  
+    buf = buf.replace(/license: .*/gm, 'license: '+ '"' + arg.service.license + '"' + ',');
+    buf =  buf.replace(/service_id: .*/gm, 'service_id: ' + '"' + arg.service.user_id+ '"' +',');
+    fs.writeFileSync(arg.conf ,buf, 'utf8');
+
+    console.log('[INFO]: Waiting until nodemon restarts the server.....')
+    await new Promise(done => setTimeout(done, 3000));
+  }
+  return true;
+}
+
+export function get_allowedCaps()
+{
+  return services[index]? services[index].caps: null;
+}
+
+/*FIXME: increment service config index */
+export function next_srv()
+{
+  index++;
+  return true;
+}
+
+const SRV = {
+  get_license:		get_license,
+  create_service:       create_service,
+  get_service:          get_service,
+  get_sii:          	get_sii,
+  get_config:           get_config,
+  update_config:        update_config,
+  //
+  get_allowedCaps:      get_allowedCaps,
+  next_srv:             next_srv,
+}
+
+export default SRV;
---
+++ framework.test/module/platform/config/clients.js
@@ -0,0 +1,23 @@
+export const clients = [
+{
+  "cii": {
+    "name": "emr.test",
+    "desc": "EMR test client",
+    "address": {
+      "phone_number": "0919161628",
+      "email": "emr.test@bokri.xyz"
+    }
+  },
+  "caps": [ 
+    "/app/emr/practner",
+    "/app/emr/mru",
+    "/app/emr/infotics",
+    "/app/emr/lab",
+    "/app/emr/pharmacy",
+    "/app/emr/finance",
+    "/app/emr/triage",
+    "/app/emr/payment",
+    "/app/emr/notif"
+  ]
+}
+];
