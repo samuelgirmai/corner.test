561e0dcc09dee2d7c3fb4373c60c64fcd7b4962f
Yared <yared.tye@gmail.com>
2019-09-18 16:46:27 +0300

    - Performance test scenarios(json scripts) added for all emr services  - Custom emr logic added ./artillery/logic/emr.js  - Load test benchmarks logged (NB: the scripts run on laptop), ./artillery/output
      -  to run the tests
        $ artillery run ./scripts/infotics.json
    
     - Check README.txt for more info

---
+++ artillery/config/config.js
@@ -0,0 +1,11 @@
+module.exports  = {
+ auth: {
+    license: "510115088897"
+  },
+  proxy: {
+    url: "http://127.0.0.1:22000",
+  },
+  stream: {
+    url: "http://127.0.0.1:22003"
+  }
+};
---
+++ artillery/logic/emr.js
@@ -0,0 +1,126 @@
+'use strict';
+
+var c = require('../config/config');
+
+function get_auth(context, ee, done) 
+{
+  context.vars.auth = c.auth;
+
+  return done();
+}
+
+function get_pii(context, ee, done)
+{
+  context.vars.param = {
+    pii: {
+      name: "Solomon",
+      fname: "Leul",
+      mname: "Zemzem",
+      mfname: "Gidey",
+      gender: "M",
+      dob: "12/12/1999",
+      address: {
+        region: "Tigray",
+        zone: "Debub",
+        woreda: "Azebo",
+        kebele: "11",
+        hous_no: "122",
+        phone_number: "09"+Math.random().toString().slice(2,10)
+      }
+    }
+  }
+  
+  return done(); 
+}
+
+function get_idata(context, ee, done)
+{
+  context.vars.param = {
+    type: "allergy",
+    data: {
+      category: "Drug Allergy " ,
+      name: "Adhesive tape" ,
+      reaction: ["Cough"]
+    }
+  }
+  return done();
+   
+}
+
+function get_exam(context, ee, done)
+{
+  context.vars.exam = {
+    repeat: false,
+    chief_compliant: 'headache',
+    allergy: [],
+    vitalsign: []
+  } 
+  
+  return done();  
+}
+
+function get_assign(context, ee, done)
+{
+  context.vars.status = 'Blue';
+
+  return done();
+}
+
+function get_opd(context, ee, done)
+{
+  context.vars.dii = {
+    catagory: 'test',
+    name: 'pediatrics',
+      address: {
+      phone_number: "09"+Math.random().toString().slice(2,10)
+    }
+  }
+
+  return done();
+}
+
+function get_result(context, ee, done)
+{
+  context.vars.result = {
+    order: {
+      catagory: 'default',
+        type: {}
+    }
+  }
+
+  return done();
+}
+
+function get_dispense(context, ee, done)
+{
+  context.vars.dispense = {
+    info: {
+      instruction: "one per day"
+    }
+  }
+
+  return done();
+}
+
+/*
+ * Just for Debugging
+ */
+function log_response(requestParams, response, context, ee, next)
+{
+
+  console.log('Log: ', response.body);
+
+  next();
+}
+
+module.exports = {
+  get_pii:      get_pii,
+  get_auth:     get_auth,
+  get_idata:    get_idata,
+  get_result:   get_result,
+  get_dispense: get_dispense,
+  get_exam:     get_exam,
+  get_assign:   get_assign,
+  get_opd:      get_opd, 
+  log_response: log_response
+}
